e20b2c677b93e5225f83c4afdac66ae0b265efc7
CJINBNH <alex_egorov_89@mail.ru>
2022-08-16 20:24:48 +0400

    Completed task001

--- task001/Program.cs
+++ task001/Program.cs
@@ -1,1 +1,10 @@
-﻿// 
+﻿// напишите программу, которая принимает на вход числа А и выдает сумму чисел от 1 до А
+
+Console.Write("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+for (int i = 1; i <= A; i++)
+{
+    sum = sum + i;
+}
+Console.WriteLine($"Сумма чисел от 1 до А: {sum}");
