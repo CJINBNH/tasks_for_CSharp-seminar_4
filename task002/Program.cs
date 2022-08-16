c6c5e1b568f42f9f00d2d5cc181cca16fc5ae74f
CJINBNH <alex_egorov_89@mail.ru>
2022-08-16 21:07:44 +0400

    Completed task002 in two ways

---
+++ task002/Program.cs
@@ -0,0 +1,25 @@
+﻿// напишите программу, которая принимает на вход число и выдает количество цифр в числе
+
+// Console.Clear();
+// Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// string toString = number.ToString();
+// int numberLength = toString.Length;
+// Console.WriteLine(numberLength);
+
+//второй вариант
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int Cycle(int number)
+{
+    int count = 0;
+    while (number > 0)
+    {
+        number = number / 10;
+        count += 1;
+    }
+    return count;
+}
+Console.WriteLine($"В числе {Cycle(number)} цифр");
