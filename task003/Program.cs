170b613d912972604cd44aff18ffb382dc199639
CJINBNH <alex_egorov_89@mail.ru>
2022-08-16 21:40:20 +0400

    Completed task003 with the function

---
+++ task003/Program.cs
@@ -0,0 +1,16 @@
+﻿// напишите программу, которая принимает на вход число N и выдает произведение чисел от 1 до N
+
+Console.Clear();
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int product = 1;
+int Cycle(int N)
+{
+    for (int i = 1; i < N; i++)
+    {
+        product = product * (i+1);
+    }
+    return product;
+}
+if (N == 0) product = 0;
+Console.WriteLine($"Произведение от 1 до N равно {Cycle(N)}");
