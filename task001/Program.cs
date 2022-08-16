3056a7d4d267591ae46531d8ab9a169b79269915
CJINBNH <alex_egorov_89@mail.ru>
2022-08-16 20:33:19 +0400

    Added a solution using the function

--- task001/Program.cs
+++ task001/Program.cs
@@ -1,10 +1,27 @@
 ﻿// напишите программу, которая принимает на вход числа А и выдает сумму чисел от 1 до А
 
+// Console.Write("Введите число: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
+// for (int i = 1; i <= A; i++)
+// {
+//     sum = sum + i;
+// }
+
+
+//решение с помощью функции
+
 Console.Write("Введите число: ");
 int A = Convert.ToInt32(Console.ReadLine());
-int sum = 0;
-for (int i = 1; i <= A; i++)
+int cycle(int a)
 {
-    sum = sum + i;
+    int sum = 0;
+    for (int i = 1; i <= A; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
 }
+int sum = cycle(A);
+
 Console.WriteLine($"Сумма чисел от 1 до А: {sum}");
